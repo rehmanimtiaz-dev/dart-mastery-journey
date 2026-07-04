fe26493bd506814b689628933251a4dc9f9d64e4
Rehman Imtiaz <rehmanimtiaz.dev@gmail.com>
2026-07-04 10:53:36 +0500

    Initial project upload

---
+++ bin/dart_journey.dart
@@ -0,0 +1,5 @@
+import 'package:dart_journey/dart_journey.dart' as dart_journey;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_journey.calculate()}!');
+}
---
+++ test/dart_journey_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_journey/dart_journey.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
